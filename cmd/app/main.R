67a1d9327e7ec52a306b28141330d2ff45c57a4f
BFinteRest <160657885+BFinteRest@users.noreply.github.com>
2025-04-25 23:54:23 +0300

    update:add etl scheduler

--- cmd/app/main.R
+++ cmd/app/main.R
@@ -11,4 +11,17 @@ dispatcher <- updater$dispatcher
 
 source("src/telegram/handlers/msg_handler.R")
 
+etl_scheduler <- function() {
+  while (TRUE) {
+    source("src/db/etl/extract_data.R")
+    source("src/db/etl/transform_data.R")
+    source("src/db/etl/load_data.R")
+    Sys.sleep(10 * 60)
+  }
+}
+
+if (requireNamespace("parallel", quietly = TRUE)) {
+  parallel::mcparallel(etl_scheduler())
+}
+
 updater$start_polling()
